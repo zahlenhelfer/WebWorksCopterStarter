9effabe25f5b13c8629721e3aa5705e8cf02b5b9
Marcus Ross <marcus.ross@zahlenhelfer.de>
2013-02-05 12:49:14 +0100

    Added small fixes and the extension

--- js/app.js
+++ js/app.js
@@ -5,35 +5,44 @@ $('#flightmode').live('touchstart touchend', function(event) {
 	  {
 		case 'trim':
 		  currentCmd = 'trim';
+		  event.preventDefault();
 	  	  break;
 	  	case 'moveForward':
 		  currentCmd = 'moveForward';
+          event.preventDefault();
 	  	  break;
 	  	case 'moveBackward':
 		  currentCmd = 'moveBackward';
+          event.preventDefault();
 	  	  break;
 	  	case 'rotateLeft':
 		  currentCmd = 'rotateLeft';
+          event.preventDefault();
 	  	  break;
 	  	case 'rotateRight':
 		  currentCmd = 'rotateRight';
+          event.preventDefault();
 	  	  break;
 	  	case 'start':
 		  currentCmd = 'trim';
           console.log('--- Starting UDP-Command-Interval ---');
           intRef = setInterval(doCurrentCmd,30);
+		  event.preventDefault();
 	      break;
 	  	case 'takeoff':
 		  currentCmd = 'takeoff';
+          event.preventDefault();
 	      break;
 	  	case 'land':
 		  currentCmd = 'land';
+     	  event.preventDefault();
 	      break;
 		default:
 		}
   } else {
     console.log("touchend: "+event.target.id);
-      currentCmd = 'hover';  
+      currentCmd = 'hover';
+      event.preventDefault();
 }
 });
 	
